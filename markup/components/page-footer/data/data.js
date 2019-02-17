c19a7b22e3ad038208ee16783cce02625ac7105e
Andrey Karamnoff <karamnoff@gmail.com>
2019-02-17 18:35:21 +0300

    add markup for page-header, socials

---
+++ markup/components/page-footer/data/data.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+const data = {
+    pageFooter: {
+        hashtag: '#visitsedona',
+        socialsList: [
+            { name: 'twitter', href: 'https://twitter.com/?lang=ru' },
+            { name: 'facebook', href: 'https://ru-ru.facebook.com' },
+            { name: 'youtube', href: 'https://www.youtube.com/?gl=RU&hl=ru' }
+        ],
+        copyright: 'Разработано'
+    }
+};
