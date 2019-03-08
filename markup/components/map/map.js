f0a51a1ef340d8aa278689a9a365288ede5cf146
Andrey Karamnoff <karamnoff@gmail.com>
2019-03-08 09:55:26 +0300

    move search' map to popup

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -1,28 +1,64 @@
-/* eslint-disable no-undef, no-console */
+/* eslint-disable no-undef, no-unused-vars */
+import $ from 'jquery';
 
+const $mfp = $.magnificPopup.instance;
+
+/**
+ * Инициализирует карту
+ */
 const init = () => {
+    let myMap;
     const map = document.querySelector('.map');
-    const myMap = new ymaps.Map(map, {
-        center: [map.dataset.latitude, map.dataset.longitude],
-        zoom: [8],
-        controls: []
-    });
-    const myPlacemark = new ymaps.Placemark(
-        myMap.getCenter(),
-        {
-            hintContent:
-                'Седона, Коконино-Каунти, штат Аризона, Соединённые Штаты Америки'
-        },
-        {
-            iconLayout: 'default#image',
-            iconImageHref: '../static/img/assets/map/map__marker.svg',
-            iconImageSize: [30, 30],
-            iconImageOffset: [-15, -15]
+    const searchBtn = document.querySelector('.search__btn');
+
+    /**
+     * Создает карту
+     */
+    const createMap = evt => {
+        evt.stopPropagation();
+
+        if (!myMap) {
+            myMap = new ymaps.Map(map, {
+                center: [map.dataset.latitude, map.dataset.longitude],
+                zoom: [8],
+                controls: []
+            });
+
+            const myPlacemark = new ymaps.Placemark(
+                myMap.getCenter(),
+                {
+                    hintContent:
+                        'Седона, Коконино-Каунти, штат Аризона, Соединённые Штаты Америки'
+                },
+                {
+                    iconLayout: 'default#image',
+                    iconImageHref: '../static/img/assets/map/map__marker.svg',
+                    iconImageSize: [30, 30],
+                    iconImageOffset: [-15, -15]
+                }
+            );
+            myMap.behaviors.disable('scrollZoom');
+            myMap.controls.add('zoomControl');
+            myMap.geoObjects.add(myPlacemark);
         }
-    );
-    myMap.behaviors.disable('scrollZoom');
-    myMap.controls.add('zoomControl');
-    myMap.geoObjects.add(myPlacemark);
+    };
+
+    /**
+     * Уничтожает карту, если popup не активен
+     */
+    const destroyMap = evt => {
+        if (myMap && !$mfp.isOpen) {
+            myMap.destroy();
+            myMap = null;
+        }
+    };
+
+    if (searchBtn) {
+        searchBtn.addEventListener('click', evt => createMap(evt));
+    }
+
+    document.addEventListener('click', evt => destroyMap(evt));
+    window.addEventListener('resize', evt => destroyMap(evt));
 };
 
 if (typeof ymaps === 'object') {
--- markup/components/search/search.js
+++ markup/components/search/search.js
@@ -1,6 +1,21 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 import $ from 'jquery';
 
 $(document).ready(() => {
-    // $('.search__btn').magnificPopup();
+    // let enableMagnific = true;
+    const $searchBtn = $('.search__btn');
+    $searchBtn.magnificPopup({
+        type: 'inline',
+        closeBtnInside: false,
+        midClick: true
+        // disableOn: () => enableMagnific
+    });
+
+    $(window).resize(() => {
+        const $mfp = $.magnificPopup.instance;
+        if ($mfp.isOpen) {
+            $mfp.close();
+            // enableMagnific = false;
+        }
+    });
 });
