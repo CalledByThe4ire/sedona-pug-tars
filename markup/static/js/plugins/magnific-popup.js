54e65ed75b3b1a681a6c51da463b2ed3b9885dfa
Andrey Karamnoff <karamnoff@gmail.com>
2019-03-24 18:13:52 +0300

    Update 2019-03-24T15:13:40.572Z

--- markup/static/js/plugins/magnific-popup.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-unused-vars */
-import $ from 'jquery';
-// https://dimsemenov.com/plugins/magnific-popup/documentation.html
-import 'magnific-popup/dist/jquery.magnific-popup';
--- markup/static/js/plugins/pixel-glass.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-// https://github.com/yoksel/pixel-glass-js
-import 'pixel-glass/script';
